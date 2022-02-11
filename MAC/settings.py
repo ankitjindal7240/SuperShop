faa49a1908d36661a6585792a8d7133fd7588ef6
Ankit <ankitjindal8787@gmail.com>
2022-02-11 10:14:43 +0530

    fixed errors

--- MAC/settings.py
+++ MAC/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+hou@s0^@&6tdfts3rv3w3qxnhn-9*342yzl5w6w!01ns@(i5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','herokuapp.com','supershopp.herokuapp.com']
 
